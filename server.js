014e427030f1fe6481128e46d9c83da71c82d9e9
Sungkwang Lee <gwangyi.kr@gmail.com>
2020-10-26 17:37:09 +0100

    advanced host allowlist

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var port = process.env.PORT || 8080;
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
 var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+var hostAllowlist = parseEnvList(process.env.CORSANYWHERE_HOSTALLOWLIST);
 function parseEnvList(env) {
   if (!env) {
     return [];
@@ -23,6 +24,7 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
+  hostAllowlist: hostAllowlist,
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
